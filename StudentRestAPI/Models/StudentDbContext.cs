9e3c15e2487601dbf53a34017aea7dc72ec1bbed
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2021-11-18 14:00:10 +0700

    working with PostgreSQL 9.5
    add UseSerialColumns

--- StudentRestAPI/Models/StudentDbContext.cs
+++ StudentRestAPI/Models/StudentDbContext.cs
@@ -6,45 +6,46 @@ using System.Threading.Tasks;
 
 namespace StudentRestAPI.Models
 {
-    public class StudentDbContext : DbContext
+  public class StudentDbContext : DbContext
+  {
+    public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
     {
-        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
-        {
 
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Student>()
-                .HasData(
-                    new Student()
-                    {
-                        Id = 1,
-                        FirstName = "Zaky",
-                        LastName = "Ramadhan",
-                        IPK = 3.50M
-                    },
-                    new Student()
-                    {
-                        Id = 2,
-                        FirstName = "Devina",
-                        LastName = "Ramadhani",
-                        IPK = 4.00M
-                    },
-                    new Student()
-                    {
-                        Id = 3,
-                        FirstName = "Putri",
-                        LastName = "Larasati",
-                        IPK = 3.35M
-                    }
-                );
-
-            modelBuilder.Entity<Student>(e => e.Property(o => o.IPK).HasColumnType("decimal(4,2)").HasConversion<decimal>());
-        }
-
-        public DbSet<Student> Student { get; set; }
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<Student>()
+          .HasData(
+              new Student()
+              {
+                Id = 1,
+                FirstName = "Zaky",
+                LastName = "Ramadhan",
+                IPK = 3.50M
+              },
+              new Student()
+              {
+                Id = 2,
+                FirstName = "Devina",
+                LastName = "Ramadhani",
+                IPK = 4.00M
+              },
+              new Student()
+              {
+                Id = 3,
+                FirstName = "Putri",
+                LastName = "Larasati",
+                IPK = 3.35M
+              }
+          );
+
+      modelBuilder.UseSerialColumns(); // important for postgres!!
+      modelBuilder.Entity<Student>(e => e.Property(o => o.IPK).HasColumnType("decimal(4,2)").HasConversion<decimal>());
+    }
+
+    public DbSet<Student> Student { get; set; }
+  }
 }
