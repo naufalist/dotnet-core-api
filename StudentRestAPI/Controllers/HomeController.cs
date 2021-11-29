5dc8c06de7a95c2bf0a76b2831a4f212a251a21c
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2021-11-29 13:21:02 +0700

    add basic route (/)
    test purpose only

---
+++ StudentRestAPI/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace StudentRestAPI.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Root()
+        {
+            object message = new
+            {
+                message = "Welcome to the jungle :)"
+            };
+            return Ok(JsonSerializer.Serialize(message));
+        }
+    }
+}
