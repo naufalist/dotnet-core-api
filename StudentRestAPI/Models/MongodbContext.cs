19b6f9e00b84c57b426b416ca0ebf04430e9b8ed
Muhammad Naufal <46437079+naufalist@users.noreply.github.com>
2021-12-27 16:03:27 +0700

    crud w/ MongoDB
    create db context

---
+++ StudentRestAPI/Models/MongodbContext.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRestAPI.Models
+{
+    public class MongodbContext
+    {
+        private readonly IMongoDatabase _db;
+
+        public MongodbContext(IMongoClient mongoClient, string databaseName)
+        {
+            _db = mongoClient.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<StudentOutput> Students => _db.GetCollection<StudentOutput>("students");
+    }
+}
